983c6d9b73aa4f1e1774d6f342e642215e2f27cf
Tiago Silva <tiago.toya@hotmail.com>
2020-05-07 15:23:42 -0300

    fix: versão de produção

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ApiWeather.MixProject do
     [
       app: :api_weather,
       version: "0.1.0",
-      elixir: "~> 1.5.3",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
