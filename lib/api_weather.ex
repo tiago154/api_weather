ea61eba8468a67730993033627d64cd7a17c0a2b
Tiago Silva <tiago.toya@hotmail.com>
2020-05-18 21:41:17 -0300

    refactor: removendo docs

--- lib/api_weather.ex
+++ lib/api_weather.ex
@@ -4,7 +4,6 @@ defmodule ApiWeather.Router do
 
   @welcome_message "Welcome to ApiWeather!"
 
-  plug(Plug.Static, at: "/docs", from: {:api_weather, "priv/static/doc/"})
   plug(:match)
   plug(ResponseContentType)
   plug(:dispatch)
@@ -12,12 +11,6 @@ defmodule ApiWeather.Router do
   get "/" do
     version = "\nversion: #{:application.get_key(:api_weather, :vsn) |> elem(1)}"
     send_resp(conn, 200, @welcome_message <> version)
-  end
-
-  get "/docs" do
-    conn
-    |> put_resp_header("location", "/docs/index.html")
-    |> send_resp(302, "/docs")
   end
 
   forward("/city/:city", to: ApiWeather.Controllers.City)
--- test/api_weather_test.exs
+++ test/api_weather_test.exs
@@ -19,22 +19,6 @@ defmodule ApiWeatherTest do
     assert response.resp_body == expected_body
   end
 
-  test "/docs redirect to documentation" do
-    expected_body = "/docs"
-
-    expected_headers = [
-      {"cache-control", "max-age=0, private, must-revalidate"},
-      {"content-type", "application/json; charset=utf-8"},
-      {"location", "/docs/index.html"}
-    ]
-
-    response = conn(:get, "/docs") |> Router.call(@opts)
-
-    assert response.status == 302
-    assert response.resp_body == expected_body
-    assert response.resp_headers == expected_headers
-  end
-
   test "/*random* page return 404" do
     expected_body = "Requested page not found!"
 
