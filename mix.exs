407b195eb620a6208396c053c984e72914bb5397
Tiago Silva <tiago.toya@hotmail.com>
2025-10-29 15:23:13 -0300

    Update mix docs

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule ApiWeather.MixProject do
       {:plug_cowboy, "~> 2.7"},
       {:httpoison, "~> 2.2"},
       {:earmark, "~> 1.2"},
-      {:ex_doc, "~> 0.19"},
+      {:ex_doc, "~> 0.30", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
       {:mock, "~> 0.3.4", only: :test},
       {:mox, "~> 0.5.2", only: :test}
